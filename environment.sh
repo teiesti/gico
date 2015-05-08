a9cfbd00ca27ac0f3dfdc89132a14ba76219eb4f
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2015-05-08 06:45:22 +0200

    gico-configure-packages.sh implemented.

--- environment.sh
+++ environment.sh
@@ -4,3 +4,4 @@ DIR=/usr/local/etc/gico			# path to the configuration repository
 RESOURCE_DIR=$DIR/res			# path where installable resources where stored
 BACKUP_DIR=$DIR/backup			# path where existing but deprecated resources with the same name were backed up
 HOOK_DIR=$DIR/hooks				# path where executable hooks where stored
+PACKAGES_FILE=$DIR/packages		# file that know whichs packages needed to be installed or uninstalled
---
+++ gico-configure-packages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Load environment variables
+source environment.sh
+
+# Install or uninstall packages
+echo "Configuring packages as defined in $PACKAGES_FILE."
+for $QUALIFIED_PACKAGE in $(cat $PACKAGES_FILE) do
+	$JOB=$(QUALIFIED_PACKAGE:0:1)
+	$PACKAGE=$(QUALIFIED_PACKAGE:1)
+	
+	if [ $JOB -eq "+" ]; then
+		echo "Installing $PACKAGE."
+		apt-get -y install $PACKAGE
+	elif [ $JOB -eq "-" ]; then
+		echo "Removing $PACKAGE."
+		apt-get remove $PACKAGE
+	elif [ $JOB -eq "_" ]; then
+		echo "Purging $PACKAGE."
+		apt-get purge $PACKAGE
+	else
+		$PACKAGE=$QUALIFIED_PACKAGE
+		echo "Installing $PACKAGE."
+		apt-get install $PACKAGE
+	fi	
+done

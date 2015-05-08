be8eaccde619a49e202582953012d33b1b5ec00f
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2015-05-08 08:09:05 +0200

    Bugfixes.

--- gico.sh
+++ gico.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 # Check if a command is given
-if [ $# -lt 1 ] ]; then
+if [ $# -lt 1 ]; then
 	./gico-help.sh
 	exit
 fi
 
 # Execute command
-if [ $1 -eq "configure"]; then
+if [ $1 = "configure" ]; then
 	if [ $# -eq 1 ]; then
 		./gico-configure.sh
 	else
 		./gico-help.sh
 	fi
-elif [ $1 -eq "configure-packages"]; then
+elif [ $1 = "configure-packages" ]; then
 	if [ $# -eq 1 ]; then
 		./gico-configure-packages.sh
 	else
 		./gico-help.sh
 	fi
-fi
-elif [ $1 -eq "full-install"]; then
+elif [ $1 = "full-install" ]; then
 	if [ $# -eq 2 ]; then
 		./gico-full-install.sh $2
 	elif [ $# -eq 3 ]; then
@@ -28,15 +27,13 @@ elif [ $1 -eq "full-install"]; then
 	else
 		./gico-help.sh
 	fi
-fi
-elif [ $1 -eq "full-update"]; then
+elif [ $1 = "full-update" ]; then
 	if [ $# -eq 1 ]; then
 		./gico-full-update.sh
 	else
 		./gico-help.sh
 	fi
-fi
-elif [ $1 -eq "install"]; then
+elif [ $1 = "install" ]; then
 	if [ $# -eq 2 ]; then
 		./gico-install.sh $2
 	elif [ $# -eq 3 ]; then
@@ -44,15 +41,13 @@ elif [ $1 -eq "install"]; then
 	else
 		./gico-help.sh
 	fi
-fi
-elif [ $1 -eq "self-install"]; then
+elif [ $1 = "self-install" ]; then
 	if [ $# -eq 1 ]; then
 		./gico-self-install.sh
 	else
 		./gico-help.sh
 	fi
-fi
-elif [ $1 -eq "update"]; then
+elif [ $1 = "update" ]; then
 	if [ $# -eq 1 ]; then
 		./gico-update.sh
 	else

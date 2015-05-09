f6910b495f08a072791d6b41d820a24d67bcba93
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2015-05-09 19:43:15 +0200

    Repository version corrected.

--- environment.sh
+++ environment.sh
@@ -6,5 +6,5 @@ RESOURCE_DIR=$DIR/res					# path where installable resources where stored
 BACKUP_DIR=$DIR/backup					# path where existing but deprecated resources with the same name were backed up
 HOOK_DIR=$DIR/hooks						# path where executable hooks where stored
 PACKAGES_FILE=$DIR/packages				# file that know whichs packages needed to be installed or uninstalled
-REPO_VERSION="v1.0-rc1"					# version of the configuration repository
+REPO_VERSION="v1.0.0-rc1"				# version of the configuration repository
 REPO_EMPTY_URL="https://github.com/teiesti/gico-empty-repository"	# url where an empty configuration repository can be found

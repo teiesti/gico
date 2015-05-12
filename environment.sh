f5fa74967ae2081a0d1dde2b16aff32a7c839923
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2015-05-12 23:58:48 +0200

    Existing files will be backed up into a folder with the current date and time.

--- environment.sh
+++ environment.sh
@@ -3,7 +3,7 @@
 BIN_DIR=/usr/local/bin					# path to the bin folder
 DIR=/usr/local/etc/gico					# path to the configuration repository
 RESOURCE_DIR=$DIR/res					# path where installable resources where stored
-BACKUP_DIR=$DIR/backup					# path where existing but deprecated resources with the same name were backed up
+BACKUP_DIR=$DIR/backup/$(date +%F_%T)	# path where existing but deprecated resources with the same name were backed up
 HOOK_DIR=$DIR/hooks						# path where executable hooks where stored
 PACKAGES_FILE=$DIR/packages				# file that know whichs packages needed to be installed or uninstalled
 REPO_VERSION="v1.0.0-rc2"				# version of the configuration repository

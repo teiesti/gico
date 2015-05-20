cef7369a8f4b8eb703d1f19e57ea627d8caf93ec
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2015-05-20 17:23:53 +0200

    gico-init.sh reworked.

--- gico-init.sh
+++ gico-init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Load environment variables
-HOME_DIR=$(dirname $(readlink -f $BASH_SOURCE))	# could not be loaded within environment.sh because of chicken and egg
-source $HOME_DIR/environment.sh
+HOME=$(dirname $(readlink -f $BASH_SOURCE))	# could not be loaded within environment.sh because of chicken and egg
+source $HOME/environment.sh
 
 # Check if Git is installed
 if [ ! -x $(which git) ]; then
@@ -11,28 +11,28 @@ if [ ! -x $(which git) ]; then
 fi
 
 # Check if the directory for the configuration repository is empty
-if [ -e $DIR ] && [ "$(ls -A $DIR)" ]; then
-	>&2 echo "Error! $DIR is not empty."
+if [ -e $REPO ] && [ "$(ls -A $REPO)" ]; then
+	>&2 echo "Error! '$REPO' is not empty."
 	exit
 fi
 
 # Creating a new gico repository
 echo "Creating repository."
 
-echo "Cloning $REPO_EMPTY_URL to $DIR."
-git clone -b $REPO_VERSION --no-checkout --depth 1 $REPO_EMPTY_URL $DIR
+echo "Cloning '$REPO_EMPTY_URL' to '$REPO'."
+git clone -b $REPO_VERSION --no-checkout --depth 1 $REPO_EMPTY_URL $REPO
 
 if [ $# -eq 0 ]; then
-	git -C $DIR checkout -b $(hostname)
+	git -C $REPO checkout -b $(hostname)
 elif [ $# -eq 1 ]; then
-	git -C $DIR checkout -b $1
+	git -C $REPO checkout -b $1
 else
 	# This should never happen because such errors are caught by gico.sh
 	>&2 echo "Error! Wrong number of arguments."
 fi
 
 echo "Rewriting commit message."
-git -C $DIR commit -q --amend -m "Basic repository structure for gico added."
+git -C $REPO commit -q --amend -m "Basic repository structure for gico added."
 
 echo "Removing remote origin."
-git -C $DIR remote rm origin
+git -C $REPO remote rm origin

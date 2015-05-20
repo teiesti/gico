bb7fee5d5ddbb3b33ca5008da0f052cbc05e48d0
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2015-05-20 17:32:05 +0200

    Deprecated files deleted.

--- old/gico-full-clone.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Load environment variables
-HOME_DIR=$(dirname $(readlink -f $BASH_SOURCE))	# could not be loaded within environment.sh because of chicken and egg
-#source $HOME_DIR/environment.sh
-
-$HOME_DIR/gico-clone.sh $@
-$HOME_DIR/gico-manage-packages.sh

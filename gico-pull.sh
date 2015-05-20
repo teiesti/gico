1f3c7a3775d69c4b31f70bc12509b795802ef9ec
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2015-05-20 17:14:09 +0200

    gico-update.sh reworked and renamed to gico-pull.sh.

--- gico-pull.sh
+++ gico-pull.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Load environment variables
-HOME_DIR=$(dirname $(readlink -f $BASH_SOURCE))	# could not be loaded within environment.sh because of chicken and egg
-source $HOME_DIR/environment.sh
+HOME=$(dirname $(readlink -f $BASH_SOURCE))	# could not be loaded within environment.sh because of chicken and egg
+source $HOME/environment.sh
 
 # Check if Git is installed
 if [ ! -x $(which git) ]; then
@@ -11,14 +11,14 @@ if [ ! -x $(which git) ]; then
 fi
 
 # Check if remote repository is specified
-if [ -z $(git -C $DIR remote) ]; then
+if [ -z $(git -C $REPO remote) ]; then
 	>&2 echo "Error! No remote repository is specified."
 	exit
 fi
 
 # Update repository
 echo "Updating repository."
-git -C $DIR pull
+git -C $REPO pull
 
 # Update links within the file system
-$HOME_DIR/gico-configure.sh
+$HOME/gico-install.sh

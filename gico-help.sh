9e1fea14efceb7de69f3cf10d49f8efb59bbe005
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2015-05-20 17:27:56 +0200

    gico-help.sh reworked.

---
+++ gico-help.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "usage: gico <command> [<arguments>]
+
+The gico commands are:
+   clone             Clone a gico repository and execute 'install'
+   help              Show this help
+   init              Create an empty gico repository
+   install           Install configuration to the system
+   pull              Update the gico repository and execute 'install'
+   self-install      Install the 'gico' command to the system
+   self-update       Update the 'gico' command to the latest version
+   
+For detailed information read the official README file."
